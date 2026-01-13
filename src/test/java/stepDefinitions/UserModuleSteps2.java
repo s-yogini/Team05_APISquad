702f9e00893df03221f227eea6bef861483c641b
PrasannaRahavi14 <Prasannarahavi14@gmail.com>
2026-01-13 16:19:57 -0500

    User2Module

--- src/test/java/stepDefinitions/UserModuleSteps2.java
+++ src/test/java/stepDefinitions/UserModuleSteps2.java
@@ -1,106 +1,87 @@
 package stepDefinitions;
-
-import org.testng.Assert;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import requests.UserRequest;
+
 
 public class UserModuleSteps2 {
 	
-	UserRequest UserRequest = new UserRequest();
-	private RequestSpecification requestSpec;
-	private Response response;
-	
-	
 	@Given("Admin has valid Bearer token")
 	public void admin_has_valid_bearer_token() {
-		requestSpec = UserRequest.setAuth();
+	   
 	    
 	}
-	
+
 	@Given("Admin creates GET Request with valid or invalid Program Id for {string}")
 	public void admin_creates_get_request_with_valid_or_invalid_program_id_for(String string) {
-		UserRequest.
-		requestSpec = UserRequest.buildRequest(requestSpec);
+	   
+	    
 	}
-	
-	@When("Admin sends HTTPS Request with endpoint")
-	public void admin_sends_https_request_with_endpoint() {
-		response = requestSpec.get(); 
+
+	@When("Admin sends HTTPS user Request with endpoint")
+	public void admin_sends_https_user_request_with_endpoint() {
+	   
 	    
 	}
 
 	@Then("Admin receives StatusCode with statusText")
 	public void admin_receives_status_code_with_status_text() {
 	   
-	    int actualStatusCode = response.getStatusCode();
-        String actualStatusText = response.getStatusLine();
-        Assert.assertEquals(actualStatusCode, 200, "Status code mismatch");  
-        Assert.assertEquals(actualStatusText, "HTTP/1.1 200 OK", "Status text mismatch");
+	    
 	}
 
-//	@Given("Admin creates GET Request with valid\\/invalid Role ID for {string}")
-//	public void admin_creates_get_request_with_valid_invalid_role_id_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates GET Request with valid\\/invalid Role ID V2 for {string}")
-//	public void admin_creates_get_request_with_valid_invalid_role_id_v2_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates GET Request with Valid User ID for {string}")
-//	public void admin_creates_get_request_with_valid_user_id_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates GET Request with valid\\/invalid User ID for {string}")
-//	public void admin_creates_get_request_with_valid_invalid_user_id_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates PUT Request to update user details for existing User ID for {string}")
-//	public void admin_creates_put_request_to_update_user_details_for_existing_user_id_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@When("Admin sends HTTPS Request and request Body")
-//	public void admin_sends_https_request_and_request_body() {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates PUT Request to update user Role ID for existing User ID for {string}")
-//	public void admin_creates_put_request_to_update_user_role_id_for_existing_user_id_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates PUT Request to assign User Role to Program\\/Batch for {string}")
-//	public void admin_creates_put_request_to_assign_user_role_to_program_batch_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates PUT Request to to update User Login Status for {string}")
-//	public void admin_creates_put_request_to_to_update_user_login_status_for(String string) {
-//	    
-//	    
-//	}
-//
-//	@Given("Admin creates DELETE Request with Valid\\/Invalid UserID for {string}")
-//	public void admin_creates_delete_request_with_valid_invalid_user_id_for(String string) {
-//	    
-//	    
-//	}
+	@Given("Admin creates GET Request with valid or invalid Role ID for {string}")
+	public void admin_creates_get_request_with_valid_or_invalid_role_id_for(String string) {
+	   
+	    
+	}
 
+	@Given("Admin creates GET Request with valid or invalid Role ID V2 for {string}")
+	public void admin_creates_get_request_with_valid_or_invalid_role_id_v2_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates GET Request with Valid User ID for {string}")
+	public void admin_creates_get_request_with_valid_user_id_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates GET Request with valid or invalid User ID for {string}")
+	public void admin_creates_get_request_with_valid_or_invalid_user_id_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates PUT Request to update user details for existing User ID for {string}")
+	public void admin_creates_put_request_to_update_user_details_for_existing_user_id_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates PUT Request to update user Role ID for existing User ID for {string}")
+	public void admin_creates_put_request_to_update_user_role_id_for_existing_user_id_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates PUT Request to assign User Role to Program\\/Batch for {string}")
+	public void admin_creates_put_request_to_assign_user_role_to_program_batch_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates PUT Request to to update User Login Status for {string}")
+	public void admin_creates_put_request_to_to_update_user_login_status_for(String string) {
+	   
+	    
+	}
+
+	@Given("Admin creates DELETE Request with valid or invalid UserID for {string}")
+	public void admin_creates_delete_request_with_valid_or_invalid_user_id_for(String string) {
+	   
+	    
+	}
+	
 }
