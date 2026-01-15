caa4158b48103a40d3e0c34273e2bbd6df3d21f9
PramodiniPatil1 <pramodini.patil182@gmail>
2026-01-15 15:43:45 -0500

    Merge team5 into Pramodini: resolved TokenManager.java conflict

--- src/test/java/utilities/TokenManager.java
+++ src/test/java/utilities/TokenManager.java
@@ -4,19 +4,19 @@ public class TokenManager {
     private static String token;
     private static String userId;
 
+    // Set token (from your branch)
+    public static void setToken(String newToken) {
+        token = newToken;
+    }
 
-	// Method to set the token
-	public static void setToken(String generatedToken) {
-	token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb2JpbnJhbmphbkBnbWFpbC5jb20iLCJpYXQiOjE3NjgzNTM3ODMsImV4cCI6MTc2ODM4MjU4M30.P73JxrJcCnYlFS-RTTFejAniz-Oq9cUOTn2nVbsqpTxhlXES5h4iLwYvato8z-RwA8CcKTJrVkOsEbE0BLN6rQ";	
-//		token = generatedToken;
-	}
-
-	// Method to get the token
-	public static String getToken() {
-		token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZWFtNUBnbWFpbC5jb20iLCJpYXQiOjE3NjgzOTkyODYsImV4cCI6MTc2ODQyODA4Nn0.uNz099lY0MWRkorogrtwWwucp9WzC_D84Vgnjh4a-SLxiDzgE425KohuPVWo5cwxYC7AH224CNQ2UCDNDtC6Kw";
-		return token;
-	}
-
+    // Get token (optionally provide default/test token if none is set)
+    public static String getToken() {
+        if (token == null || token.isEmpty()) {
+            // Optional: default token from team5 for testing
+            token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZWFtNUBnbWFpbC5jb20iLCJpYXQiOjE3NjgzOTkyODYsImV4cCI6MTc2ODQyODA4Nn0.uNz099lY0MWRkorogrtwWwucp9WzC_D84Vgnjh4a-SLxiDzgE425KohuPVWo5cwxYC7AH224CNQ2UCDNDtC6Kw";
+        }
+        return token;
+    }
 
     public static void setUserId(String newUserId) {
         userId = newUserId;
